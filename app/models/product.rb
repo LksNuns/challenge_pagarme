bf3bb7dcb02071d530b6b7e29fa995c8a192ae50
Lucas Arvani <lksnunss@gmail.com>
2016-07-12 14:34:48 -0300

    add gem annotate

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,16 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  desc       :string
+#  price      :float
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Product < ActiveRecord::Base
   belongs_to :user
-
 end
