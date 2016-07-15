7caffa2a52c9b69a348cc3291e4ebeb70f046f49
Lucas Arvani <lksnunss@gmail.com>
2016-07-15 11:40:13 -0300

    add route and action for checkout

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,15 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def checkout
+    # transaction = PagarMe::Transaction.find_by_id(params[:token])
+    # transaction.capture({:amount => 1000})
+    # >>>>Params
+    # "token"=>"test_transaction_EdubrKehhENuyTgJgTpsoWlEAuISJK", 
+    # "payment_method"=>"credit_card",
+    # "id"=>"6"
+  end
+
+
+  private
 end
