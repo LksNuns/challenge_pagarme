e24bdc1418490e2f54f138ec93d0039f7b62d86e
Lucas Arvani <lksnunss@gmail.com>
2016-07-16 13:40:02 -0300

    fix js

--- Gemfile
+++ Gemfile
@@ -9,14 +9,10 @@ gem 'uglifier', '>= 1.3.0'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 
-# Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
-# gem 'turbolinks'
-
 # Front
 gem 'bootstrap-sass', '~> 3.3.6'
 gem 'sass-rails', '>= 3.2'
 
-gem 'sprockets-rails', :require => 'sprockets/railtie'
 gem 'haml'
 
 # Payment
--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,14 +1,15 @@
-$( document ).ready(function() {
+$(document).ready(function() {
 
-  var min_amount = parseFloat($('#min_amount').text());
   var amount = $('#amount');
+  var minimum_amount = parseFloat($('#min_amount').text());
+  var value = $('form script:first').attr('data-amount', parseInt(amount.val()*100));
 
   amount.change(function(){
-    if(amount.val() < min_amount)
+    if(amount.val() < minimum_amount){
       $('.pagarme-checkout-btn').attr("disabled", true);
-    else
+    }else{
       $('form script:first').attr('data-amount', parseInt(amount.val()*100));
       $('.pagarme-checkout-btn').removeAttr("disabled");
+    }
   });
-
 });
